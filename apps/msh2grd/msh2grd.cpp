4816b2fdd88109cf9757fc5a5b512edc4c614aad
Thomas Funkhouser <funk@cs.princeton.edu>
2018-03-03 16:08:46 -0800

    msh2grd

--- apps/msh2grd/msh2grd.cpp
+++ apps/msh2grd/msh2grd.cpp
@@ -15,6 +15,7 @@ static char *grid_name = NULL;
 static int min_resolution = 16;
 static int max_resolution = 512;
 static double grid_spacing = 0.1;
+static int sdf = 0;
 static int print_verbose = 0;
 
 
@@ -52,6 +53,22 @@ ReadMesh(char *mesh_name)
 
 
 
+static RNLength
+ShortestEdgeLength(R3Mesh *mesh, R3MeshFace *face)
+{
+  // Return shortest edge on face
+  RNLength d0 = mesh->EdgeLength(mesh->EdgeOnFace(face, 0));
+  RNLength d1 = mesh->EdgeLength(mesh->EdgeOnFace(face, 1));
+  RNLength d2 = mesh->EdgeLength(mesh->EdgeOnFace(face, 2));
+  RNLength d = FLT_MAX;
+  if (d0 < d) d = d0;
+  if (d1 < d) d = d1;
+  if (d2 < d) d = d2;
+  return d;
+}
+
+
+
 static R3Grid *
 CreateGrid(R3Mesh *mesh)
 {
@@ -73,6 +90,61 @@ CreateGrid(R3Mesh *mesh)
     const R3Point& p1 = mesh->VertexPosition(mesh->VertexOnFace(face, 1));
     const R3Point& p2 = mesh->VertexPosition(mesh->VertexOnFace(face, 2));
     grid->RasterizeWorldTriangle(p0, p1, p2, 1.0);
+  }
+
+  // Make binary (in case triangles overlap)
+  grid->Threshold(0.5, 0, 1);
+
+  // Check if should compute sdf
+  if (sdf) {
+    // Compute distance grid
+    grid->SquaredDistanceTransform();
+    grid->Sqrt();
+    grid->Multiply(grid->GridToWorldScaleFactor());
+
+    // Allocate temmporary memory
+    int *votes = new int [ grid->NEntries() ];
+    int *components = new int [ grid->NEntries() ];
+    for (int i = 0; i < grid->NEntries(); i++) votes[i] = 0;
+    for (int i = 0; i < grid->NEntries(); i++) components[i] = 0;
+
+    // Vote for signs by connected component
+    R3Grid signs(*grid);
+    signs.Clear(0);
+    for (int i = 0; i < mesh->NFaces(); i++) {
+      R3MeshFace *face = mesh->Face(i);
+      RNLength e = ShortestEdgeLength(mesh, face);
+      if (e > grid_spacing) e = grid_spacing;
+      R3Point p = mesh->FaceCentroid(face);
+      R3Point p1 = p + e * mesh->FaceNormal(face);
+      R3Point p2 = p - e * mesh->FaceNormal(face);
+      signs.RasterizeWorldPoint(p1, 1.0);
+      signs.RasterizeWorldPoint(p2, -1.0);
+    }
+
+    // Vote for connected components
+    int ncomponents = grid->ConnectedComponents(0, grid->NEntries(), NULL, NULL, components);
+    if (ncomponents > 0) {
+      for (int i = 0; i < grid->NEntries(); i++) {
+        int component = components[i];
+        if (component < 0) continue;
+        votes[component] += signs.GridValue(i);
+      }
+
+      // Set signs by votes on connected components
+      for (int i = 0; i < grid->NEntries(); i++) {
+        int component = components[i];
+        if (component < 0) continue;
+        if (votes[component] < 0) {
+          RNScalar d = grid->GridValue(i);
+          grid->SetGridValue(i, -d);
+        }
+      }
+    }
+    
+    // Delete temporary memory
+    delete [] components;
+    delete [] votes;
   }
 
   // Print statistics
@@ -129,6 +201,7 @@ ParseArgs(int argc, char **argv)
   while (argc > 0) {
     if ((*argv)[0] == '-') {
       if (!strcmp(*argv, "-v")) print_verbose = 1; 
+      else if (!strcmp(*argv, "-sdf")) sdf = 1; 
       else if (!strcmp(*argv, "-spacing")) { argc--; argv++; grid_spacing = atof(*argv); }
       else if (!strcmp(*argv, "-min_resolution")) { argc--; argv++; min_resolution = atoi(*argv); }
       else if (!strcmp(*argv, "-max_resolution")) { argc--; argv++; max_resolution = atoi(*argv); }
