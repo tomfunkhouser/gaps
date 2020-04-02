95d6a43f41abf9065ad1ed04d190f2c46c75066c
Thomas Funkhouser <funk@cs.princeton.edu>
2020-04-01 18:12:58 -0700

    sflview

--- apps/sflview/map.cpp
+++ apps/sflview/map.cpp
@@ -10,7 +10,6 @@ namespace gaps {}
 using namespace gaps;
 #include "R3Graphics/R3Graphics.h"
 #include "R3Surfels/R3Surfels.h"
-#include "R3SurfelViewer.h"
 #include "map.h"
 
 
--- apps/sflview/sflview.cpp
+++ apps/sflview/sflview.cpp
@@ -11,7 +11,6 @@ using namespace gaps;
 #include "R3Graphics/R3Graphics.h"
 #include "R3Surfels/R3Surfels.h"
 #include "fglut/fglut.h" 
-#include "R3SurfelViewer.h"
 #include "align.h"
 #include "debug.h"
 
