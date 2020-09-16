1ae85eeaa5843218289fbe92338ee2660775ebfc
Thomas Funkhouser <funk@cs.princeton.edu>
2020-09-16 15:20:28 -0700

    R3Frustum

---
+++ pkgs/R3Shapes/R3Frustum.cpp
@@ -0,0 +1,370 @@
+// Source file for view frustum class
+
+
+
+////////////////////////////////////////////////////////////////////////
+// Include files
+////////////////////////////////////////////////////////////////////////
+
+#include "R3Shapes.h"
+
+
+
+// Namespace
+
+namespace gaps {
+
+
+
+////////////////////////////////////////////////////////////////////////
+// Class type definitions
+////////////////////////////////////////////////////////////////////////
+
+RN_CLASS_TYPE_DEFINITIONS(R3Frustum);
+
+
+
+////////////////////////////////////////////////////////////////////////
+// Constructors
+////////////////////////////////////////////////////////////////////////
+
+R3Frustum::
+R3Frustum(void)
+{
+  // Empty
+  Empty();
+}
+
+
+
+R3Frustum::
+R3Frustum(const R3Point& viewpoint, const R3Vector& towards, const R3Vector& up, 
+  RNAngle xfov, RNAngle yfov, RNLength neardist, RNLength fardist)
+  : cs(viewpoint, R3Triad(towards, up)),
+    xfov(xfov),
+    yfov(yfov),
+    neardist(neardist),
+    fardist(fardist)
+{
+  // Compute halfspaces
+  for (int dir = 0; dir < 2; dir++) {
+    for (int dim = 0; dim < 3; dim++) {
+      R3Vector normal;
+      switch (dim) {
+      case RN_X:
+        normal = cs.Axes().Axis(RN_X); 
+        if (dir == RN_LO) normal.Rotate(cs.Axes().Axis(RN_Y), xfov);
+        else normal.Rotate(cs.Axes().Axis(RN_Y), RN_PI - xfov);
+        halfspaces[dir][dim] = R3Halfspace(cs.Origin(), normal);
+        break;
+
+      case RN_Y:
+        normal = cs.Axes().Axis(RN_Y); 
+        if (dir == RN_LO) normal.Rotate(cs.Axes().Axis(RN_X), -yfov);
+        else normal.Rotate(cs.Axes().Axis(RN_X), yfov - RN_PI);
+        halfspaces[dir][dim] = R3Halfspace(cs.Origin(), normal);
+        break;
+
+      case RN_Z:
+        if (dir == RN_LO) {
+          halfspaces[dir][dim] = R3Halfspace(cs.Origin() - cs.Axes().Axis(RN_Z) * neardist, -cs.Axes().Axis(RN_Z));
+        }
+        else {
+          halfspaces[dir][dim] = R3Halfspace(cs.Origin() - cs.Axes().Axis(RN_Z) * fardist, cs.Axes().Axis(RN_Z));
+        }
+        break;
+
+      default:
+        RNAbort("Should never get here");
+        break;
+      }
+    }
+  }
+}
+
+
+
+////////////////////////////////////////////////////////////////////////
+// Frustum property functions
+////////////////////////////////////////////////////////////////////////
+
+const R3Point R3Frustum::
+Corner (RNOctant octant) const
+{
+  // Return corner point 
+  switch (octant) {
+  case RN_NNN_OCTANT: return Corner(RN_LO, RN_LO, RN_LO);
+  case RN_NNP_OCTANT: return Corner(RN_LO, RN_LO, RN_HI);
+  case RN_NPN_OCTANT: return Corner(RN_LO, RN_HI, RN_LO);
+  case RN_NPP_OCTANT: return Corner(RN_LO, RN_HI, RN_HI);
+  case RN_PNN_OCTANT: return Corner(RN_HI, RN_LO, RN_LO);
+  case RN_PNP_OCTANT: return Corner(RN_HI, RN_LO, RN_HI);
+  case RN_PPN_OCTANT: return Corner(RN_HI, RN_HI, RN_LO);
+  case RN_PPP_OCTANT: return Corner(RN_HI, RN_HI, RN_HI);
+  default: RNAbort("Invalid octant for frustum corner"); return R3null_point;
+  }
+}
+
+
+
+const R3Point R3Frustum::
+Corner(int xdir, int ydir, int zdir) const
+{
+  // Return corner point
+  double xsign = (xdir == RN_LO) ? -1 : 1;
+  double ysign = (ydir == RN_LO) ? -1 : 1;
+  double depth = (zdir == RN_LO) ? Near() : Far();
+  R3Point corner = cs.Origin() - cs.Axes().Axis(RN_Z) * depth;
+  corner += cs.Axes().Axis(RN_X) * depth * tan(xfov) * xsign;
+  corner += cs.Axes().Axis(RN_Y) * depth * tan(yfov) * ysign;
+  return corner;
+}
+  
+
+  
+////////////////////////////////////////////////////////////////////////
+// Shape property functions
+////////////////////////////////////////////////////////////////////////
+
+const RNBoolean R3Frustum::
+IsPoint(void) const
+{
+  return FALSE;
+}
+
+
+
+const RNBoolean R3Frustum::
+IsLinear(void) const
+{
+  return FALSE;
+}
+
+
+ 
+const RNBoolean R3Frustum::
+IsPlanar(void) const
+{
+  return FALSE;
+}
+
+
+
+const RNBoolean R3Frustum::
+IsConvex(void) const 
+{
+  return TRUE;
+}
+
+
+
+const RNInterval R3Frustum::
+NFacets(void) const
+{
+  return RNInterval(6,6);
+}
+
+
+
+const RNArea R3Frustum::
+Area(void) const
+{
+  // Todo
+  return 0;
+}
+
+
+
+const RNVolume R3Frustum::
+Volume(void) const
+{
+  // Todo
+  return 0;
+}
+
+
+
+const R3Point R3Frustum::
+Centroid(void) const
+{
+  // Return centroid
+  double middepth = 0.5 * (Near() + Far());
+  return cs.Origin() - middepth * cs.Axes().Axis(RN_Z);
+}
+
+
+
+const R3Point R3Frustum::
+ClosestPoint(const R3Point& point) const
+{
+  // Intialize closest point
+  R3Point closest_point = point;
+
+  // Project onto each halfspace
+  for (int dir = 0; dir < 2; dir++) {
+    for (int dim = 0; dim < 3; dim++) {
+      const R3Plane& plane = halfspaces[dir][dim].Plane();
+      RNScalar d = R3SignedDistance(plane, point);
+      if (d < 0) closest_point -= d*plane.Normal();
+    }
+  }
+
+  // Return closest point
+  return closest_point;
+}
+
+
+
+const R3Point R3Frustum::
+FurthestPoint(const R3Point& point) const
+{
+  // Todo
+  return R3unknown_point;
+}
+
+
+
+const R3Shape& R3Frustum::
+BShape(void) const
+{
+  // Return self
+  return *this;
+}
+
+
+
+const R3Box R3Frustum::
+BBox(void) const
+{
+  // Return bounding box
+  R3Box bbox = R3null_box;
+  for (int i = 0; i < 2; i++) {
+    for (int j = 0; j < 2; j++) {
+      for (int k = 0; k < 2; k++) {
+        bbox.Union(Corner(i, j, k));
+      }
+    }
+  }
+  return bbox;
+}
+
+
+
+
+const R3Sphere R3Frustum::
+BSphere(void) const
+{
+  // Return bounding sphere
+  R3Point centroid = Centroid();
+  R3Point corner = Corner(RN_HI, RN_HI, RN_HI);
+  RNLength radius = R3Distance(centroid, corner);
+  return R3Sphere(centroid, radius);
+}
+
+
+ 
+////////////////////////////////////////////////////////////////////////
+// Manipulation functions
+////////////////////////////////////////////////////////////////////////
+
+void R3Frustum::
+Empty(void)
+{
+  // Reset coordinate system
+  cs = R3xyz_coordinate_system;
+
+  // Reset fovs
+  xfov = 0;
+  yfov = 0;
+  
+  // Reset halfspaces
+  for (int dir = 0; dir < 2; dir++) {
+    for (int dim = 0; dim < 3; dim++) {
+      halfspaces[dir][dim] = R3null_halfspace;
+    }
+  }
+}
+
+
+
+void R3Frustum::
+Transform(const R3Transformation& transformation)
+{
+  // Assumes rigid transformation
+  // xfov, yfov stay same
+
+  // Transform coordinate system
+  cs.Transform(transformation);
+
+  // Transform halfspaces
+  for (int dir = 0; dir < 2; dir++) {
+    for (int dim = 0; dim < 3; dim++) {
+      halfspaces[dir][dim].Transform(transformation);
+    }
+  }
+}
+
+
+
+void R3Frustum::
+Draw(void) const
+{
+  // Draw quads
+  glBegin(GL_QUADS);
+  R3LoadPoint(Corner(0,0,0));  // Front
+  R3LoadPoint(Corner(1,0,0));
+  R3LoadPoint(Corner(1,1,0));
+  R3LoadPoint(Corner(0,1,0));
+  R3LoadPoint(Corner(0,0,1));  // Back
+  R3LoadPoint(Corner(0,1,1));
+  R3LoadPoint(Corner(1,1,1));
+  R3LoadPoint(Corner(1,0,1));
+  R3LoadPoint(Corner(1,0,0)); // Right
+  R3LoadPoint(Corner(1,0,1));
+  R3LoadPoint(Corner(1,1,1));
+  R3LoadPoint(Corner(1,1,0));
+  R3LoadPoint(Corner(0,0,0));  // Left
+  R3LoadPoint(Corner(0,0,1));
+  R3LoadPoint(Corner(0,1,1));
+  R3LoadPoint(Corner(0,1,0));
+  R3LoadPoint(Corner(0,1,0));  // Top
+  R3LoadPoint(Corner(1,1,0));
+  R3LoadPoint(Corner(1,1,1));
+  R3LoadPoint(Corner(0,1,1));
+  R3LoadPoint(Corner(0,0,0));  // Bottom
+  R3LoadPoint(Corner(0,0,1));
+  R3LoadPoint(Corner(1,0,1));
+  R3LoadPoint(Corner(1,0,0));
+  glEnd();
+}
+
+
+
+void R3Frustum::
+Outline(void) const
+{
+  // Draw wireframe
+  glBegin(GL_LINE_STRIP);
+  R3LoadPoint(Corner(0,0,0));
+  R3LoadPoint(Corner(1,0,0));
+  R3LoadPoint(Corner(1,1,0));
+  R3LoadPoint(Corner(0,1,0));
+  R3LoadPoint(Corner(0,0,0));
+  R3LoadPoint(Corner(0,0,1));
+  R3LoadPoint(Corner(1,0,1));
+  R3LoadPoint(Corner(1,1,1));
+  R3LoadPoint(Corner(0,1,1));
+  R3LoadPoint(Corner(0,0,1));
+  R3LoadPoint(Corner(0,0,0));
+  R3LoadPoint(Corner(1,0,0));
+  R3LoadPoint(Corner(1,0,1));
+  R3LoadPoint(Corner(1,1,1));
+  R3LoadPoint(Corner(1,1,0));
+  R3LoadPoint(Corner(0,1,0));
+  R3LoadPoint(Corner(0,1,1));
+  glEnd();
+}
+
+
+
+} // namespace gaps
---
+++ pkgs/R3Shapes/R3Frustum.h
@@ -0,0 +1,143 @@
+// Include file for R3 view frustum class
+#ifndef __R3__FRUSTUM__H__
+#define __R3__FRUSTUM__H__
+
+
+
+/* Begin namespace */
+namespace gaps {
+
+
+
+////////////////////////////////////////////////////////////////////////
+// Class definition
+////////////////////////////////////////////////////////////////////////
+
+struct R3Frustum : public R3Solid {
+public:
+  // Constructor
+  R3Frustum(void);
+  R3Frustum(const R3Point& viewpoint,
+    const R3Vector& towards, const R3Vector& up,
+    RNAngle xfov, RNAngle yfov, 
+    RNLength neardist, RNLength fardist);
+
+  // Property functions
+  const R3Halfspace& Halfspace(int dir, int dim) const;
+  const R3CoordSystem& CoordSystem(void) const;
+  const R3Point Corner(RNOctant octant) const;
+  const R3Point Corner(int xdir, int ydir, int zdir) const;
+  const RNAngle XFov(void) const; 
+  const RNAngle YFov(void) const; 
+  const RNLength Near(void) const; 
+  const RNLength Far(void) const; 
+
+  // Shape propetry functions/operators
+  virtual const RNBoolean IsPoint(void) const;
+  virtual const RNBoolean IsLinear(void) const ;
+  virtual const RNBoolean IsPlanar(void) const ;
+  virtual const RNBoolean IsConvex(void) const ;
+  virtual const RNInterval NFacets(void) const;
+  virtual const RNArea Area(void) const;
+  virtual const RNVolume Volume(void) const;
+  virtual const R3Point Centroid(void) const;
+  virtual const R3Point ClosestPoint(const R3Point& point) const;
+  virtual const R3Point FurthestPoint(const R3Point& point) const;
+  virtual const R3Shape& BShape(void) const;
+  virtual const R3Box BBox(void) const;
+  virtual const R3Sphere BSphere(void) const;
+
+  // Manipulation functions
+  virtual void Empty(void);
+  virtual void Transform(const R3Transformation& transformation);
+  virtual void SetHalfspace(int dir, int dim, const R3Halfspace& halfspace);
+
+  // Draw function
+  void Draw(void) const;
+  void Outline(void) const;
+
+  // Standard shape definitions
+  RN_CLASS_TYPE_DECLARATIONS(R3Frustum);
+  R3_SHAPE_RELATIONSHIP_DECLARATIONS(R3Frustum);
+
+public:
+  R3CoordSystem cs;
+  R3Halfspace halfspaces[2][3];
+  RNAngle xfov, yfov;
+  RNLength neardist, fardist;
+};
+
+
+
+// Inline functions
+
+inline const R3CoordSystem& R3Frustum::
+CoordSystem(void) const
+{
+  // Return coordinate system for frustum
+  // (apex at origin, -z points towards, +x points right, +y points up)
+  return cs;
+}
+
+
+
+inline const RNAngle R3Frustum::
+XFov(void) const
+{
+  // Return half angle between planes
+  return xfov;
+}
+
+
+
+inline const RNAngle R3Frustum::
+YFov(void) const
+{
+  // Return half angle between planes
+  return yfov;
+}
+
+
+
+inline const RNLength R3Frustum::
+Near(void) const
+{
+  // Return near distance  
+  return neardist;
+}
+
+
+
+inline const RNLength R3Frustum::
+Far(void) const
+{
+  // Return near distance  
+  return fardist;
+}
+
+
+
+inline const R3Halfspace& R3Frustum::
+Halfspace(int dir, int dim) const
+{
+  // Return halfspace
+  return halfspaces[dir][dim];
+}
+
+
+
+inline void R3Frustum::
+SetHalfspace(int dir, int dim, const R3Halfspace& halfspace)
+{
+  // Set halfspace
+  this->halfspaces[dir][dim] = halfspace;
+}
+
+
+
+// End namespace
+}
+
+
+// End include guard
+#endif
